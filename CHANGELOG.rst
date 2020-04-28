9f1e40febaa0fb969909c27041dfa0a362a72414
Francesco Ceccon <francesco@ceccon.me>
2020-04-28 10:40:21 +0100

    Bump version to 0.2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,7 +1,14 @@
 Changelog
 =========
 
+0.2.0
+-----
+
+ * Rewrite using Pybind11
+ * General performance improvements
+
+
 0.1.0
 -----
 
-* Initial release
+ * Initial release
--- cppyad/__version__.py
+++ cppyad/__version__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 __author__ = 'Francesco Ceccon'
 __author_email__ = 'francesco@ceccon.me'
 __description__ = 'Python bindings to Coin-OR CppAD'
